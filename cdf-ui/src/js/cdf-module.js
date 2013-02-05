79dac75675fe7b879d9c65c85c202b541000b397
Marius Giepz <mgiepz@googlemail.com>
2013-02-05 17:53:32 +0100

    init

---
+++ cdf-ui/src/js/cdf-module.js
@@ -0,0 +1,21 @@
+define([
+  'cdf/jquery',
+  'cdf/jquery.ui',
+  'cdf/jquery-impromptu.3.1',
+  'cdf/jquery.bgiframe',
+  'cdf/jquery.blockUI',
+  'cdf/jquery.corner',
+  'cdf/jquery.eventstack',
+  'cdf/jquery.i18n.properties',
+  'cdf/jquery.jdMenu',
+  'cdf/jquery.positionBy',
+  'cdf/jquery.sparkline',
+  'cdf/jquery.tooltip',
+  'cdf/json',
+  'cdf/simile/ajax/simile-ajax-api',
+  'cdf/simile/ajax/scripts/json',
+  'cdf/Base',
+  'cdf/Dashboards',
+  'cdf/CoreComponents'
+], function(){
+});
